178d4d0564503c7661c92c7621648afce30b3923
LucasDuarteV <lucasdv2733@gmail.com>
2026-05-29 14:04:19 -0300

    subindo projeto good

---
+++ Course/Course/Entities/Reservention.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Course.Entities.Expeceptions;
+namespace Course.Entities
+{
+    class Reservention
+    {
+        public int RoomNumber { get; set; }
+        public DateTime Checkin { get; set; }
+        public DateTime Checkout { get; set; }
+
+        public Reservention()
+        {
+
+        }
+
+        public Reservention(int roomNumber, DateTime checkKin, DateTime checkOut)
+        {
+            if (checkOut < checkKin)
+            {
+                throw new DomainException("Error Chekin-in maior que Chech-out");
+            }
+
+            RoomNumber = roomNumber;
+            Checkin = checkKin;
+            Checkout = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = Checkout.Subtract(Checkin);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkKin , DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+
+            if (checkKin < now || checkOut < now)
+            {
+                throw new DomainException("Error check-in e chech-out menor que now");
+
+            }
+
+            if (checkOut < checkKin)
+            {
+               throw new DomainException("Error Chekin-in maior que Chech-out");
+            }
+
+            Checkin = checkKin;
+            Checkout = checkOut;
+
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                +", check-in: "
+                + Checkin.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + Checkout.ToString("dd/MM/yyyy")
+                +", "
+                + Duration()
+                +" nights";
+            
+        }
+        
+    }
+}
---
+++ Course/Course/Program.cs
@@ -0,0 +1,68 @@
+﻿using Course.Entities;
+using System;
+using System.Collections.Generic; 
+using System.ComponentModel;
+using System.Data.SqlTypes;
+using System.Diagnostics.Contracts;
+using System.Globalization;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Transactions;
+using System.Xml;
+using Course.Entities.Expeceptions;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.Write("Room number: ");
+                int number = int.Parse(Console.ReadLine()!);
+
+                Console.Write("Check-int data (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine()!);
+
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine()!);
+
+
+                Reservention reserve = new Reservention(number, checkIn, checkOut);
+                Console.WriteLine();
+                Console.WriteLine(reserve);
+                Console.WriteLine();
+                Console.WriteLine("UPDATE RESERVE: ");
+
+                Console.Write("Check-int data (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine()!);
+
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine()!);
+
+                reserve.UpdateDates(checkIn, checkOut);
+
+                Console.WriteLine();
+
+                Console.WriteLine("UPDATE: " + reserve);
+            }  
+
+            catch (DomainException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           catch(FormatException e) 
+            {
+                Console.WriteLine("Format error " + e.Message);
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
